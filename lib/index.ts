66f275cb2f332d5731d01d340e7644e2a432f7e4
김창회 <dudnqnfqlc@naver.com>
2021-09-19 03:10:29 +0900

    fix init 함수 안으로 기존 함수 이동

--- lib/index.ts
+++ lib/index.ts
@@ -1,55 +1,38 @@
 import { data } from "./data";
 import { QuoteType, TagType } from "../@types/type";
 
-const _shuffle = (array: QuoteType[]) => {
-    return array[Math.floor(Math.random() * array.length)];
-};
-
-const _get = (quotes: QuoteType[]) => {
-    return (tag?: TagType) => {
-        if (tag) {
-            return quotes.filter((lst) => lst.tag === tag);
-        }
-        return quotes;
+const init = () => {
+    const _shuffle = (array: QuoteType[]) => {
+        return array[Math.floor(Math.random() * array.length)];
     };
-};
 
-const _random = (quotes: QuoteType[]) => {
-    return (tag?: TagType) => {
-        if (tag) {
-            const filter = quotes.filter((lst) => lst.tag === tag);
-            return _shuffle(filter);
-        }
-        return _shuffle(quotes);
+    const _get = (quotes: QuoteType[]) => {
+        return (tag?: TagType) => {
+            if (tag) {
+                return quotes.filter((lst) => lst.tag === tag);
+            }
+            return quotes;
+        };
     };
+
+    const _random = (quotes: QuoteType[]) => {
+        return (tag?: TagType) => {
+            if (tag) {
+                const filter = quotes.filter((lst) => lst.tag === tag);
+                return _shuffle(filter);
+            }
+            return _shuffle(quotes);
+        };
+    };
+
+    const quotes = data;
+    const random = _random(quotes);
+    const get = _get(quotes);
+
+    return { random, get };
 };
 
-const quotes = data;
-
-/**
- * random() 함수는 명언 중 하나를 랜덤으로 반환 합니다.
- * @function random(tag?: 1 | 2 | 3)
- *
- * const quote = kadvice.random();
- *
- * tag 번호에 따라 해당하는 태그의 명언 중 하나를 갖고 올 수도있습니다.
- *
- * const quote = kadvice.random(1);
- */
-const random = _random(quotes);
-/**
- * get() 함수는 리스트를 반환합니다. 기본 형식은 다음과 같습니다.
- * @function get(tag?: 1 | 2 | 3)
- *
- * const quotes = kadvice.get();
- *
- * tag 번호에 따라 명언의 유형을 달리 불러올 수도 있습니다.
- *
- * const quotes = kadvice.get(1);
- */
-const get = _get(quotes);
-
-export { random, get };
+export const { random, get } = init();
 
 /**
  * 한국어로 된 명언 모음입니다.
