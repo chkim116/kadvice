eec4983e6146999b0fc65bb7dae06b0c147726fc
김창회 <dudnqnfqlc@naver.com>
2021-09-19 03:06:25 +0900

    fix function explain

--- lib/index.ts
+++ lib/index.ts
@@ -24,45 +24,55 @@ const _random = (quotes: QuoteType[]) => {
     };
 };
 
-/**
- * 한국어로 된 명언 모음입니다.
- */
 const quotes = data;
+
 /**
- * 1-3까지의 태그타입을 인자로 넣으면 태그에 해당하는 명언 중 하나를 불러옵니다.
+ * random() 함수는 명언 중 하나를 랜덤으로 반환 합니다.
  * @function random(tag?: 1 | 2 | 3)
  *
- * const quote = random()
+ * const quote = kadvice.random();
  *
+ * tag 번호에 따라 해당하는 태그의 명언 중 하나를 갖고 올 수도있습니다.
+ *
+ * const quote = kadvice.random(1);
  */
 const random = _random(quotes);
 /**
- * 1-3까지의 태그타입을 인자로 넣으면 태그에 해당하는 명언을 불러옵니다.
- *
- * @function get(tag?: 1 | 2 | 3)
- *
- * const quotes = get()
- */
-const get = _get(quotes);
-
-export { quotes, random, get };
-
-/**
- * 한국어로 된 명언 모음입니다.
- *
- * const quotes = kadvice.quotes;
- *
- * ---
- * tag 번호에 따라 명언의 유형을 달리 부를 수 있습니다.
- *
- * 1 = 삶 관련 2 = 동기부여 관련 3 = 그외
- *
+ * get() 함수는 리스트를 반환합니다. 기본 형식은 다음과 같습니다.
  * @function get(tag?: 1 | 2 | 3)
  *
  * const quotes = kadvice.get();
  *
+ * tag 번호에 따라 명언의 유형을 달리 불러올 수도 있습니다.
+ *
+ * const quotes = kadvice.get(1);
+ */
+const get = _get(quotes);
+
+export { random, get };
+
+/**
+ * 한국어로 된 명언 모음입니다.
+ *
+ * ---
+ * 기본 형식은 다음과 같습니다.
+ * @function get(tag?: 1 | 2 | 3)
+ *
+ * const quotes = kadvice.get();
+ *
+ * tag 번호에 따라 명언의 유형을 달리 불러올 수도 있습니다.
+ *
+ * const quotes = kadvice.get(1);
+ *
+ *  ---
+ *
+ * random() 함수는 기본적으로 하나를 랜덤으로 불러옵니다.
  * @function random(tag?: 1 | 2 | 3)
  *
  * const quote = kadvice.random();
+ *
+ * tag 번호에 따라 해당하는 태그의 명언 중 하나를 갖고 올 수도있습니다.
+ *
+ * const quote = kadvice.random(1);
  */
-export default { quotes, random, get };
+export default { random, get };
